8798002dbaac0934786a4b5b0df269e0cc8fcd5b
Assma1504 <assmabaziz15@gamil.com>
2026-05-03 13:57:40 +0300

    first task completed

---
+++ task_one.py
@@ -0,0 +1,26 @@
+class Restaurant():
+    def __init__(self, restaurantName, restaurantType):
+        self.restaurantName= restaurantName
+        self.restaurantType = restaurantType
+    
+    def describe_restaurant(self, restaurantSurface, numberWorkers):
+        self.restaurantSurface = restaurantSurface
+        self.numberWorkers = numberWorkers
+        print(f"Our restaurant has a surface equal of: {restaurantSurface}, in this restaurant work: {numberWorkers}")
+
+    def open_restaurant(self):
+        print("Welcome to our restaurant")
+
+
+firstNewRestaurant = Restaurant("Chicken Foo", "Fast food")
+secondNewRestaurant = Restaurant("Amazigh", "Algerian kitchen")
+
+print(f"The first restaurant is named {firstNewRestaurant.restaurantName}, its type is {firstNewRestaurant.restaurantType}")
+print(f"The second restaurant is named {secondNewRestaurant.restaurantName}, its type is {secondNewRestaurant.restaurantType}")
+
+firstNewRestaurant.describe_restaurant("140", 35)
+secondNewRestaurant.describe_restaurant("180", 40)
+
+firstNewRestaurant.open_restaurant()
+secondNewRestaurant.open_restaurant()
+
